aa67df64699e6c208ef442bf367709228a29f39b
bovenzit2681 <bovenzit2681@CA-SU-F106-21.sduhsd.lan>
2019-03-13 10:33:39 -0700

    Created Unit 12

---
+++ src/Monster/Skeleton.java
@@ -0,0 +1,19 @@
+package Monster;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import Monster.Monster;
+import static java.lang.System.*; 
+
+public class Skeleton implements Monster
+{
+	//add instance variables	
+
+	//add a constructor
+
+	//add code to implement the Monster interface
+
+	//add a toString
+}
